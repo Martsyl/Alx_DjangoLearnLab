26e5c97f69e5bfca821c748ba37cdb285c411ac5
martsyl <msnonuorah3964@gmail.com>
2026-01-19 00:15:54 +0100

    tired

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -11,24 +11,32 @@ class Author(models.Model):
         return self.name
 
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(
-        Author,
-        on_delete=models.CASCADE,
-        related_name='books'
-    )
-
-    def __str__(self):
-        return self.title
-
-
 class Library(models.Model):
     name = models.CharField(max_length=150)
-    books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(
+        Library,
+        on_delete=models.CASCADE,
+        related_name='books'  # <-- No clash now, Library.books refers to all books in that library
+    )
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
 
 
 class Librarian(models.Model):
