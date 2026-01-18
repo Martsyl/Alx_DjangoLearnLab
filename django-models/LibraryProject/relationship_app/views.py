2eb4f4402131b5ce7eb7dc76137d44386f5d939f
martsyl <msnonuorah3964@gmail.com>
2026-01-18 23:37:54 +0100

    task 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,9 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
+
 
 def list_books(request):
     books = Book.objects.all()
@@ -38,3 +41,24 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
