e6041a207c70565a98f1bf0e4475d6875ee60ad5
martsyl <msnonuorah3964@gmail.com>
2026-01-18 23:56:29 +0100

    task 3.6

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,9 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import user_passes_test
 
-from .models import Book, Library
+from .models import Book
+from .models import Library
+
 
 # -----------------------
 # Books & Library
