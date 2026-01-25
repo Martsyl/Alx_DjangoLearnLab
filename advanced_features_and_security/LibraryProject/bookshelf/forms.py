5de2f5f9113763c63c429b81f8ecd60e09ec9c9a
martsyl <msnonuorah3964@gmail.com>
2026-01-25 22:31:46 +0100

    task3 testing1

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,8 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author']
+from django import forms
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=200)
+    author = forms.CharField(max_length=100)
