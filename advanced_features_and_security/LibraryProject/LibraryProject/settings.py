c5ce73df155eb2107060b1f96f4b274506ed3cd0
martsyl <msnonuorah3964@gmail.com>
2026-01-25 22:53:30 +0100

    task4 testing1

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -140,3 +140,5 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 
 # Enable browser XSS protection
 SECURE_BROWSER_XSS_FILTER = True
+# Tell Django it’s behind a proxy that handles HTTPS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
