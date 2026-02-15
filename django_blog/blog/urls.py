7d0679f7be04b45cc18ec2e838b75c2bcf782e32
martsyl <msnonuorah3964@gmail.com>
2026-02-15 23:07:39 +0100

    commenterror

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,6 +11,7 @@ from .views import (
 from .views import (
     CommentUpdateView,
     CommentDeleteView,
+    CommentCreateView,
     add_comment,
 )
 
@@ -34,6 +35,8 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
       path('post/<int:pk>/comments/new/', add_comment, name='add-comment'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
+
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
 
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -113,6 +113,30 @@ def add_comment(request, pk):
             comment.save()
 
     return redirect('post-detail', pk=post.pk)
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['content']
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        form.instance.author = self.request.user
+        form.instance.post = post
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'blog/comment_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = 'blog/comment_confirm_delete.html'
